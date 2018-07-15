0f802fd930cf41c3393c77fd3275dcfe7c0734a1
Leonardo Ardjomand <leo.ardjomand@gmail.com>
2018-07-15 19:19:02 -0300

    Replace ActionBar with Toolbar

--- app/src/main/java/ardjomand/leonardo/nutrimeal/MainActivity.java
+++ app/src/main/java/ardjomand/leonardo/nutrimeal/MainActivity.java
@@ -3,6 +3,7 @@ package ardjomand.leonardo.nutrimeal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
