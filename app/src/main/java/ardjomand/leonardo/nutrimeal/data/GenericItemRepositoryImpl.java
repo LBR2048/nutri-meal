e001c542090afe16bc059f5550fb80b2202f206c
LBR2048 <leo.ardjomand@gmail.com>
2018-09-17 15:32:58 +0200

    refactor: Create repository to monitor specific value in Firebase node

---
+++ app/src/main/java/ardjomand/leonardo/nutrimeal/data/GenericItemRepositoryImpl.java
@@ -0,0 +1,56 @@
+package ardjomand.leonardo.nutrimeal.data;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+public class GenericItemRepositoryImpl<T extends KeyClass> implements GenericItemRepository.Repository {
+
+    private static final String TAG = GenericItemRepositoryImpl.class.getSimpleName();
+
+    private final GenericItemRepository.Presenter<T> presenter;
+    private final Class<T> clazz;
+
+    private DatabaseReference itemsRef;
+    private ValueEventListener valueEventListener;
+
+    public GenericItemRepositoryImpl(GenericItemRepository.Presenter<T> presenter, Class<T> clazz) {
+        this.presenter = presenter;
+        this.clazz = clazz;
+
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
+
+        itemsRef = firebaseHelper.getItemsRef(clazz);
+    }
+
+    @Override
+    public void subscribe(String key) {
+        if (valueEventListener == null) {
+            valueEventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    T cartMeal = dataSnapshot.getValue(clazz);
+                    if (cartMeal != null){
+                        cartMeal.setKey(dataSnapshot.getKey());
+                        presenter.onItemRead(cartMeal);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+
+            itemsRef.addValueEventListener(valueEventListener);
+        }
+    }
+
+    @Override
+    public void unsubscribe() {
+        if (valueEventListener != null) {
+            itemsRef.removeEventListener(valueEventListener);
+        }
+    }
+}
